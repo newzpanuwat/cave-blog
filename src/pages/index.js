90a7c1af9fbb9fea07eda1d22de75e701f968383
New Panuwat <pfcreatorit@gmail.com>
2019-01-14 02:38:39 +0700

    add more content index pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,32 @@
 import React from "react"
 import Layout from "../components/layout"
+import { italic } from "ansi-colors";
 
 export default () => (
     <Layout>
-    <h1>Hi! Caveman!</h1>
-    <p>
-      พวกข้าคือมะนุดถ้ำผู้ยิ่งใหญ่ พวกเจ้าจงเกรงกลัวพวกข้า!!!  อูกะ อูกะ !!
-    </p>
+    <h1>Baby bamboo!</h1>
     <img
         src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
         alt="Group of pandas eating bamboo"
       />
+    <p style={{ fontStyle: `bold`, fontSize: `1.2rem` }}>
+      baby_bamboo
+    </p>
+    <p>
+      พวกข้าคือมะนุดถ้ำผู้ยิ่งใหญ่ พวกเจ้าจงเกรงกลัวพวกข้า!!!  อูกะ อูกะ !!
+    </p>
+
+    <h1>Cold...</h1>
+    <img
+        src="https://images.unsplash.com/photo-1547155101-7c3284a615d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        alt="Cat"
+      />
+    <p style={{ fontStyle: `bold`, fontSize: `1.2rem` }}>
+      alone_2018
+    </p>
+    <p>
+     หนาวจริม ๆ!!!  อูกะ อูกะ !!
+    </p>
+    
     </Layout>
 )
