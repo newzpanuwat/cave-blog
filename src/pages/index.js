72d478d1dcd8eea1ed0484d0a57ba4e062c9de46
New Panuwat <pfcreatorit@gmail.com>
2019-01-14 02:09:31 +0700

    first repo gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+    <h1>Hi! Caveman!</h1>
+    <p>
+      พวกข้าคือมะนุดถ้ำผู้ยิ่งใหญ่ พวกเจ้าจงเกรงกลัวพวกข้า!!!  อูกะ อูกะ !!
+    </p>
+    <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </Layout>
+)
